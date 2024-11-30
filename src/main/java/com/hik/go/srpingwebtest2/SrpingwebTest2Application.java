e93560dbbca274f606cb2fa2bb89af2976c8e0cf
dlsrud0108 <dlsrud0108@yonsei.ac.kr>
2024-12-01 01:03:16 +0900

    test1

---
+++ src/main/java/com/hik/go/srpingwebtest2/SrpingwebTest2Application.java
@@ -0,0 +1,13 @@
+package com.hik.go.srpingwebtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingwebTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingwebTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hik/go/srpingwebtest2/SrpingwebTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hik.go.srpingwebtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingwebTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
